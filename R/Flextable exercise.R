69212d36fbc0765adb63389c305203eadb73340f
Sarah Whiteside <sarah.whiteside@emory.edu>
2025-08-18 16:54:06 -0400

    continued practice with flextables

--- R/Flextable exercise.R
+++ R/Flextable exercise.R
@@ -10,3 +10,21 @@ ft <- add_footer_lines(ft, "Daily air quality measurements in New York, May to S
 ft <- color(ft, part = "footer", color = "#666666")
 ft <- set_caption(ft, caption = "New York Air Quality Measurements")
 ft
+
+#defining the formatting for your flextables
+set_flextable_defaults(
+	font.size = 10, theme_fun = theme_vanilla,
+	padding = 6,
+	background.color = "#EFEFEF")
+flextable(cars)
+
+
+#1.4: Walkthrough: simple example
+myft <- flextable(head(mtcars),
+									col_keys = c("am", "carb", "gear", "mpg", "drat" ))
+myft
+
+myft <- fontsize(mfyt, size=20)
+myft
+
+
