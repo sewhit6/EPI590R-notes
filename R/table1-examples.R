e6a83e89350931782b6fc7f768f557fc6862bbb0
Sarah Whiteside <sarah.whiteside@emory.edu>
2025-08-18 15:54:04 -0400

    working on tables

--- R/table1-examples.R
+++ R/table1-examples.R
@@ -70,7 +70,7 @@ tbl_summary(
     all_categorical() ~ "chisq.test"
   )) |>
   # add a total column with the number of observations
-  add_overall(col_label = "**Total** N = {N}") |>
+  add_overall(col_label = "**Total**<br>N = {N}") |>
   bold_labels() |>
   # remove the default footnotes
   modify_footnote(update = everything() ~ NA) |>
@@ -97,8 +97,7 @@ tbl_summary(
 )
 
 # Exercise, Step 4
-# Stratify the table by sex. Add a p-value comparing the sexes and an overall
-#column combining both sexes.
+# Stratify the table by sex. Add a p-value comparing the sexes and an overall column combining both sexes.
 tbl_summary(
 	nlsy,
 	by = sex_cat,
@@ -112,7 +111,9 @@ tbl_summary(
 		income ~ "Income",
 		sleep_wkdy ~ "Sleep on weekdays",
 		sleep_wknd ~ "Sleep on weekends"
-	)) |>
+	))|>
+	add_overall(col_label = "**Sex**<br>N = {N}")
+	bold_labels() |>
 	add_p(test = list(
 		all_continuous() ~ "t.test",
 		all_categorical() ~ "chisq.test"
@@ -134,16 +135,16 @@ tbl_summary(
 		income ~ "Income",
 		sleep_wkdy ~ "Sleep on weekdays",
 		sleep_wknd ~ "Sleep on weekends"
-	),
+	))|>
+	add_overall(col_label = "**Sex**<br>N = {N}")
+	bold_labels() |>
 	statistic = list(
 		income ~ "{p90}, {p10}",
-		sleep_wkdy ~ "min = {min}; max={max}",
-		sleep_wkdy ~ "min = {min}; max={max}",
+		starts_with("sleep") ~ "min = {min}; max={max}",
 		digits = list(
-			income ~ style_sigfig(3),
-			sleep_wkdy ~ style_sigfig(1),
-			sleep_wknd ~ style_sigfig(1)
-		))) |>
+			income ~ c(3, 3),#onefor p90 and one for p10
+			starts_with("sleep") ~ c(1,1) #1 for min and 1 for max
+		)) |>
 	add_p(test = list(
 		all_continuous() ~ "t.test",
 		all_categorical() ~ "chisq.test"
@@ -152,3 +153,37 @@ tbl_summary(
 #Exercise, Step 6
 # Add a footnote to the race/ethnicity variable with a link to the page describing how
 #NLSY classified participants: https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data
+tbl_summary(
+		nlsy,
+		by = sex_cat,
+		include = c(
+			race_eth_cat, region_cat, income,
+			starts_with("sleep")
+		),
+		label=list(
+			race_eth_cat ~ "Race/ethnicity",
+			region_cat ~ "Region",
+			income ~ "Income",
+			sleep_wkdy ~ "Sleep on weekdays",
+			sleep_wknd ~ "Sleep on weekends"
+		))|>
+		add_overall(col_label = "**Sex**<br>N = {N}")
+	bold_labels() |>
+		statistic = list(
+			income ~ "{p90}, {p10}",
+			starts_with("sleep") ~ "min = {min}; max={max}",
+			digits = list(
+				income ~ c(3, 3),#onefor p90 and one for p10
+				starts_with("sleep") ~ c(1,1) #1 for min and 1 for max
+			)) |>
+		add_p(test = list(
+			all_continuous() ~ "t.test",
+			all_categorical() ~ "chisq.test"
+		) |>
+			# figured out how to do this from
+			# https://stackoverflow.com/questions/73154658/adding-a-footnote-to-a-single-row-label-in-a-gtsummary-table
+			modify_table_styling(
+				columns = label,
+				rows = label == "Race/ethnicity",
+				footnote = "see https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data"
+			))
